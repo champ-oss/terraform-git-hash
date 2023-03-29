86c3bf3063d0bcdc2dac44f3a7104483882f68ff
Justin Reeves <reeves122@gmail.com>
2023-03-29 15:38:12 -0500

    test multiple module calls

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -2,7 +2,16 @@ module "this" {
   source = "../../"
   path   = path.module
 }
+  
+module "this2" {
+  source = "../../"
+  path   = path.module
+}
 
 output "hash" {
   value = module.this.hash
+}
+
+output "hash2" {
+  value = module.this2.hash
 }
